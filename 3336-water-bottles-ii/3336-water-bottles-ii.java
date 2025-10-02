7e85f6bf59a4fbf6826593846c99f3a6472c984c
SAMEEP SINGH <sameep2203@gmail.com>
2025-10-02 14:16:05 +0530

    Time: 0 ms (100%), Space: 41.1 MB (38.06%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int drink = numBottles;
+         while (numExchange <= numBottles) {
+            ++drink;
+            numBottles -= numExchange;
+            ++numExchange;
+            ++numBottles;
+        }
+        return drink;
+    }
+}
